7e6ebbc218f510fde8d721dc88669a2a39b233a8
Taylor Flagg <trflagg@gmail.com>
2015-04-18 13:10:26 -0400

    initialize provinces

--- public/js/models/gameModel.js
+++ public/js/models/gameModel.js
@@ -1,4 +1,9 @@
-define(['backbone', './provinceModel'], function(Backbone, ProvinceModel) {
+define(['backbone',
+  'underscore',
+  './provinceModel'
+], function(Backbone,
+  _,
+  ProvinceModel) {
 
   gameModel = Backbone.Model.extend({
 
@@ -6,6 +11,23 @@ define(['backbone', './provinceModel'], function(Backbone, ProvinceModel) {
       this.provinces = new Backbone.Collection([], {
         model: ProvinceModel
       });
+
+      var province_ids = [
+        'Kandahar',
+        'Zabol',
+        'Helmand',
+        'Nimruz',
+        'Farah',
+        'Herat',
+        'Ghowr',
+        'Oruzgan'
+      ]
+      _.each(province_ids, function(id) {
+        var new_province = new ProvinceModel({
+          id: id
+        })
+        this.provinces.add(new_province);
+      }, this);
 
     }
   });
--- public/js/models/provinceModel.js
+++ public/js/models/provinceModel.js
@@ -3,7 +3,7 @@ define(['backbone'], function(Backbone) {
   provinceModel = Backbone.Model.extend({
 
     initialize: function() {
-
+      this.set('influence', Math.random());
     }
   });
 
