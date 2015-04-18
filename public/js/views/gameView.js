bf3ea474ce9e4308d3d5660a370ce63a1fa11293
Taylor Flagg <trflagg@gmail.com>
2015-04-18 12:35:20 -0400

    make svgView superclass

--- public/js/views/gameView.js
+++ public/js/views/gameView.js
@@ -1,16 +1,16 @@
 define([
   'svg',
   'svg.import',
-  'backbone',
+  './svgView',
   'text!../../svg/Map.svg'
 ], function(
   SVG,
   svgImport,
-  Backbone,
+  SVGView,
   map
 ) {
 
-  var gameView = Backbone.View.extend({
+  var gameView = SVGView.extend({
     id: 'gameView',
 
     initialize: function() {
@@ -25,25 +25,9 @@ define([
         group.each(function() {
             this.fill('#FF0000').stroke({color: '#000', width: 5})
         });
-        group.each(function() {
-          this.on('click', function() {
-            this.fill('#0000FF');
-          });
-        })
     },
 
-    nameSpace: "http://www.w3.org/2000/svg",
-    _ensureElement: function() {
-       if (!this.el) {
-          var attrs = _.extend({}, _.result(this, 'attributes'));
-          if (this.id) attrs.id = _.result(this, 'id');
-          if (this.className) attrs['class'] = _.result(this, 'className');
-          var $el = $(window.document.createElementNS(_.result(this, 'nameSpace'), _.result(this, 'tagName'))).attr(attrs);
-          this.setElement($el.get(0), false);
-       } else {
-          this.setElement(_.result(this, 'el'), false);
-       }
-    },
+
   })
 
   return gameView;
