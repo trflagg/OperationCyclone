f765fcd07d20bd2c948cf2866c146277b0c2146b
Taylor Flagg <trflagg@gmail.com>
2015-04-18 20:51:17 -0400

    Table supports army numbers and looks better

--- public/js/models/provinceModel.js
+++ public/js/models/provinceModel.js
@@ -12,6 +12,15 @@ define(['backbone',
       });
       this.set('influence', Math.random());
 
+      this.establishArmy();
+    },
+
+    establishArmy: function() {
+      this.set('communist_army_size', _.random(10000, 50000))
+      this.set('communist_arms_level', _.random(2, 4))
+
+      this.set('rebel_army_size', _.random(5000, 20000))
+      this.set('rebel_arms_level', _.random(1, 3))
     }
   });
 
--- public/js/views/cellView.js
+++ public/js/views/cellView.js
@@ -16,6 +16,10 @@ define(['backbone',
     },
 
     render: function() {
+      var attrs = this.model.attributes
+      attrs.withComma = function(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
       this.$el.html(this.template(this.model.attributes));
       var color = this.model.get('influence') > .5 ? '#0069A4' : '#ED0021'
       this.$el.css('border-color', color);
